1aa87114adae562dd22822d9122b9580c247f9cd
yangjinlong <yangjinlong86@gmail.com>
2016-03-27 22:26:36 +0800

    b3log solo for nocoder.org

---
+++ solo/README.md
@@ -0,0 +1,72 @@
+# [Solo](https://github.com/b3log/solo) [![Build Status](https://img.shields.io/travis/b3log/solo.svg?style=flat)](https://travis-ci.org/b3log/solo) [![Coverage Status](https://img.shields.io/coveralls/b3log/b3log-solo.svg?style=flat)](https://coveralls.io/r/b3log/b3log-solo)  [![Apache License](http://img.shields.io/badge/license-apache2-orange.svg?style=flat)](http://www.apache.org/licenses/LICENSE-2.0) [![Download](http://img.shields.io/badge/download-~5K-blue.svg?style=flat)](http://pan.baidu.com/share/link?shareid=541735&uk=3255126224)
+
+<img src="https://cloud.githubusercontent.com/assets/873584/6817919/72ffdb5c-d2e2-11e4-8be8-37ac1b08ff81.jpg"  width="96%" />
+
+## Introduction
+
+[Solo](https://github.com/b3log/solo) is a professional, simple, stable and good performance open source Java blogging system. You can setup it by yourself ([download](http://pan.baidu.com/share/link?shareid=541735&uk=3255126224)) or buy our provided service, see [here](http://b3log.org/services/#solo) for more details.
+
+[Solo](https://github.com/b3log/solo) 是一个专业、简约、稳定、极速的开源 Java 博客系统。你可以自己搭建使用（[下载](http://pan.baidu.com/share/link?shareid=541735&uk=3255126224)），也可以通过购买我们搭建好的服务来直接使用，详情请看[这里](http://b3log.org/services/#solo)。
+
+> 加入[**黑客派**](https://hacpai.com/register)，与其他程序员、设计师共同成长！
+
+## Features 
+
+* Tags
+* Atom/RSS, Sitemap
+* Comment/Reply email notification
+* Page & Post
+* Markdown/TinyMCE/KindEditor
+* Sticky/Random/Relvant post
+* Permalink
+* Draft, Sign
+* Multiple users, i18n, Themes, Plugins
+* MetaWeblog API
+
+## Screenshots
+
+* **Index - Skin (ease)**
+![skin-ease](https://cloud.githubusercontent.com/assets/873584/5584735/e1634c80-90d1-11e4-80a9-bae4ca8eb37d.png)
+* **Admin - Post (TinyMCE/Markdown)**  
+<img src="https://cloud.githubusercontent.com/assets/873584/5584725/848cdf58-90d1-11e4-8d8a-f186e05db2cd.png" width="49%" />&nbsp;&nbsp;<img src="https://cloud.githubusercontent.com/assets/873584/5585055/6bf35892-90da-11e4-986c-c80e4df5dde1.png" width="49%" />
+* **Admin - Skins**
+![admin-skins](https://cloud.githubusercontent.com/assets/873584/9975917/994d9e02-5f00-11e5-8a6d-fe652ac3e75e.png)
+
+## Top Cases
+
+Here are some our most valuable business users' cases:
+
+以下是我们的最有价值商业用户案例：
+
+* [Lanr](http://lanr.b3log.org)
+* [木子](http://369595.com)
+* [Vanessa](http://vanessa.b3log.org)
+* [简约设计の艺术](http://88250.b3log.org)
+
+## Documents
+
+* [User Guide](https://github.com/b3log/solo/wiki/standalone_mode)
+* [Dev Guide](https://github.com/b3log/solo/wiki/Pre_dev)
+  * [Skin Dev Guide](https://github.com/b3log/solo/wiki/Develop_steps)
+  * [Plugin Dev Guide](https://docs.google.com/document/pub?id=15H7Q3EBo-44v61Xp_epiYY7vK_gPJLkQaT7T1gkE64w&pli=1)
+
+## Change logs
+
+See [here](http://solo.b3log.org/CHANGE_LOGS.html).
+
+## Contact Us
+
+* Forum: https://hacpai.com
+* support@liuyun.io (purchase purpose)
+
+## Terms
+
+* This software is open sourced under the Apache License 2.0
+* You can not get rid of the "Powered by [B3log 开源](http://b3log.org)" from any page, even which you made
+* If you want to use this software for commercial purpose, please mail to support@liuyun.io for a commercial license request
+* Copyright &copy; b3log.org, all rights reserved
+
+## Credits
+
+* [Latke](https://github.com/b3log/latke)
+* [IntelliJ IDEA](http://www.jetbrains.com/idea/index.html)
---
+++ solo/src/main/java/org/b3log/solo/service/UpgradeService.java
@@ -0,0 +1,314 @@
+/*
+ * Copyright (c) 2010-2016, b3log.org & hacpai.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.b3log.solo.service;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Statement;
+import javax.inject.Inject;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.b3log.latke.Keys;
+import org.b3log.latke.Latkes;
+import org.b3log.latke.logging.Level;
+import org.b3log.latke.logging.Logger;
+import org.b3log.latke.mail.MailService;
+import org.b3log.latke.mail.MailServiceFactory;
+import org.b3log.latke.model.User;
+import org.b3log.latke.repository.*;
+import org.b3log.latke.repository.jdbc.util.Connections;
+import org.b3log.latke.service.LangPropsService;
+import org.b3log.latke.service.ServiceException;
+import org.b3log.latke.service.annotation.Service;
+import org.b3log.solo.SoloServletListener;
+import org.b3log.solo.model.*;
+import org.b3log.solo.repository.*;
+import org.b3log.solo.util.Markdowns;
+import org.b3log.solo.util.Thumbnails;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
+/**
+ * Upgrade service.
+ *
+ * @author <a href="http://88250.b3log.org">Liang Ding</a>
+ * @author <a href="mailto:dongxu.wang@acm.org">Dongxu Wang</a>
+ * @version 1.2.0.4, Dec 17, 2015
+ * @since 1.2.0
+ */
+@Service
+public class UpgradeService {
+
+    /**
+     * Logger.
+     */
+    private static final Logger LOGGER = Logger.getLogger(UpgradeService.class.getName());
+
+    /**
+     * Article repository.
+     */
+    @Inject
+    private ArticleRepository articleRepository;
+
+    /**
+     * Comment repository.
+     */
+    @Inject
+    private CommentRepository commentRepository;
+
+    /**
+     * User repository.
+     */
+    @Inject
+    private UserRepository userRepository;
+
+    /**
+     * Option repository.
+     */
+    @Inject
+    private OptionRepository optionRepository;
+
+    /**
+     * Step for article updating.
+     */
+    private static final int STEP = 50;
+
+    /**
+     * Preference Query Service.
+     */
+    @Inject
+    private PreferenceQueryService preferenceQueryService;
+
+    /**
+     * Mail Service.
+     */
+    private static final MailService MAIL_SVC = MailServiceFactory.getMailService();
+
+    /**
+     * Whether the email has been sent.
+     */
+    private static boolean sent = false;
+
+    /**
+     * Language service.
+     */
+    @Inject
+    private LangPropsService langPropsService;
+
+    /**
+     * Old version.
+     */
+    private static final String FROM_VER = "1.2.0";
+
+    /**
+     * New version.
+     */
+    private static final String TO_VER = SoloServletListener.VERSION;
+
+    /**
+     * Upgrades if need.
+     */
+    public void upgrade() {
+        try {
+            final JSONObject preference = preferenceQueryService.getPreference();
+            if (null == preference) {
+                return;
+            }
+
+            final String currentVer = preference.getString(Option.ID_C_VERSION);
+
+            if (SoloServletListener.VERSION.equals(currentVer)) {
+                return;
+            }
+
+            if (FROM_VER.equals(currentVer)) {
+                perform();
+
+                return;
+            }
+
+            LOGGER.log(Level.WARN, "Attempt to skip more than one version to upgrade. Expected: {0}; Actually: {1}", FROM_VER, currentVer);
+
+            if (!sent) {
+                notifyUserByEmail();
+
+                sent = true;
+            }
+        } catch (final Exception e) {
+            LOGGER.log(Level.ERROR, e.getMessage(), e);
+            LOGGER.log(Level.ERROR,
+                    "Upgrade failed [" + e.getMessage() + "], please contact the Solo developers or reports this "
+                    + "issue directly (<a href='https://github.com/b3log/solo/issues/new'>"
+                    + "https://github.com/b3log/solo/issues/new</a>) ");
+        }
+    }
+
+    /**
+     * Performs upgrade.
+     *
+     * @throws Exception upgrade fails
+     */
+    private void perform() throws Exception {
+        LOGGER.log(Level.INFO, "Upgrading from version [{0}] to version [{1}]....", FROM_VER, TO_VER);
+
+        Transaction transaction = null;
+
+        try {
+            final Connection connection = Connections.getConnection();
+            final Statement statement = connection.createStatement();
+            final String tablePrefix = Latkes.getLocalProperty("jdbc.tablePrefix") + "_";
+            statement.execute("DROP TABLE `" + tablePrefix + "preference`");
+            statement.close();
+            connection.commit();
+            connection.close();
+
+            transaction = optionRepository.beginTransaction();
+
+            final JSONObject versionOpt = optionRepository.get(Option.ID_C_VERSION);
+            versionOpt.put(Option.OPTION_VALUE, TO_VER);
+            optionRepository.update(Option.ID_C_VERSION, versionOpt);
+
+            final JSONArray comments = commentRepository.get(new Query()).optJSONArray(Keys.RESULTS);
+            for (int i = 0; i < comments.length(); i++) {
+                final JSONObject comment = comments.getJSONObject(i);
+
+                String name = comment.optString(Comment.COMMENT_NAME);
+                name = Jsoup.clean(name, Whitelist.none());
+                comment.put(Comment.COMMENT_NAME, name);
+
+                String content = comment.optString(Comment.COMMENT_CONTENT);
+                content = StringEscapeUtils.unescapeHtml(content).replaceAll("_esc_enter_88250_", "\n\n");
+                content = Markdowns.toHTML(content);
+                content = Jsoup.clean(content, Whitelist.relaxed());
+                comment.put(Comment.COMMENT_CONTENT, content);
+
+                commentRepository.update(comment.optString(Keys.OBJECT_ID), comment);
+            }
+
+            transaction.commit();
+
+            LOGGER.log(Level.INFO, "Updated preference");
+        } catch (final Exception e) {
+            if (null != transaction && transaction.isActive()) {
+                transaction.rollback();
+            }
+
+            LOGGER.log(Level.ERROR, "Upgrade failed!", e);
+            throw new Exception("Upgrade failed from version [" + FROM_VER + "] to version [" + TO_VER + ']');
+        }
+
+        LOGGER.log(Level.INFO, "Upgraded from version [{0}] to version [{1}] successfully :-)", FROM_VER, TO_VER);
+    }
+
+    /**
+     * Upgrades users.
+     *
+     * <p>
+     * Password hashing.
+     * </p>
+     *
+     * @throws Exception exception
+     */
+    private void upgradeUsers() throws Exception {
+        final JSONArray users = userRepository.get(new Query()).getJSONArray(Keys.RESULTS);
+
+        for (int i = 0; i < users.length(); i++) {
+            final JSONObject user = users.getJSONObject(i);
+            final String email = user.optString(User.USER_EMAIL);
+
+            user.put(UserExt.USER_AVATAR, Thumbnails.getGravatarURL(email, "128"));
+
+            userRepository.update(user.optString(Keys.OBJECT_ID), user);
+
+            LOGGER.log(Level.INFO, "Updated user[email={0}]", email);
+        }
+    }
+
+    /**
+     * Upgrades articles.
+     *
+     * @throws Exception exception
+     */
+    private void upgradeArticles() throws Exception {
+        LOGGER.log(Level.INFO, "Adds a property [articleEditorType] to each of articles");
+
+        final JSONArray articles = articleRepository.get(new Query()).getJSONArray(Keys.RESULTS);
+
+        if (articles.length() <= 0) {
+            LOGGER.log(Level.TRACE, "No articles");
+            return;
+        }
+
+        Transaction transaction = null;
+
+        try {
+            for (int i = 0; i < articles.length(); i++) {
+                if (0 == i % STEP || !transaction.isActive()) {
+                    transaction = userRepository.beginTransaction();
+                }
+
+                final JSONObject article = articles.getJSONObject(i);
+
+                final String articleId = article.optString(Keys.OBJECT_ID);
+
+                LOGGER.log(Level.INFO, "Found an article[id={0}]", articleId);
+                article.put(Article.ARTICLE_EDITOR_TYPE, "tinyMCE");
+
+                articleRepository.update(article.getString(Keys.OBJECT_ID), article);
+
+                if (0 == i % STEP) {
+                    transaction.commit();
+                    LOGGER.log(Level.TRACE, "Updated some articles");
+                }
+            }
+
+            if (transaction.isActive()) {
+                transaction.commit();
+            }
+
+            LOGGER.log(Level.TRACE, "Updated all articles");
+        } catch (final Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+
+            throw e;
+        }
+    }
+
+    /**
+     * Send an email to the user who upgrades Solo with a discontinuous version.
+     *
+     * @throws ServiceException ServiceException
+     * @throws JSONException JSONException
+     * @throws IOException IOException
+     */
+    private void notifyUserByEmail() throws ServiceException, JSONException, IOException {
+        final String adminEmail = preferenceQueryService.getPreference().getString(Option.ID_C_ADMIN_EMAIL);
+        final MailService.Message message = new MailService.Message();
+
+        message.setFrom(adminEmail);
+        message.addRecipient(adminEmail);
+        message.setSubject(langPropsService.get("skipVersionMailSubject"));
+        message.setHtmlBody(langPropsService.get("skipVersionMailBody"));
+
+        MAIL_SVC.send(message);
+
+        LOGGER.info("Send an email to the user who upgrades Solo with a discontinuous version.");
+    }
+}
