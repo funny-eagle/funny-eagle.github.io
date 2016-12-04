a8fde299105867d4cfd23805c51aa3751d6302e9
yangjinlong <yangjinlong86@gmail.com>
2016-12-04 18:21:22 +0800

    update

--- freda/src/main/java/org/nocoder/redis/RedisUtils.java
+++ freda/src/main/java/org/nocoder/redis/RedisUtils.java
@@ -1,11 +1,12 @@
 package org.nocoder.redis;
 
+import java.util.List;
+
 import org.nocoder.utils.SerializeUtil;
+
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
-
-import java.util.List;
 
 /**
  * Redis工具类
@@ -17,16 +18,12 @@ public class RedisUtils {
 
 	static{
 		JedisPoolConfig config = new JedisPoolConfig();
-        //控制一个pool可分配多少个jedis实例，通过pool.getResource()来获取；
-        //如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
         config.setMaxTotal(500);
-        //控制一个pool最多有多少个状态为idle(空闲的)的jedis实例。
         config.setMaxIdle(5);
-        //表示当borrow(引入)一个jedis实例时，最大的等待时间，如果超过等待时间，则直接抛出JedisConnectionException；
         config.setMaxWaitMillis(100000);
-        //在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
-        pool = new JedisPool(config, "127.0.0.1", 6379, 100000);
+        pool = new JedisPool(config, "192.168.1.16", 6379, 100000);
 	}
+	
     public static void setList(String key, List<?> list){
         getJedis().set(key.getBytes(), SerializeUtil.serializeList(list));
     }
@@ -37,7 +34,13 @@ public class RedisUtils {
     }
 
     public static Jedis getJedis(){
-        Jedis jedis = pool.getResource();
+    	Jedis jedis = pool.getResource();
+    	jedis.auth("123456");
         return jedis;
     }
+    
+    public static void main(String[] args) {
+    	Jedis jedis = RedisUtils.getJedis();
+    	jedis.set("aaa","bbb");
+	}
 }
