24e388eabba2a949decfdd265465306c677f5840
yangjinlong <yangjinlong@tzpt.com>
2018-09-26 14:25:43 +0800

    service exception

---
+++ freda/src/main/java/org/nocoder/blog/exception/ServiceException.java
@@ -0,0 +1,44 @@
+package org.nocoder.blog.exception;
+
+/**
+ * Service Exception
+ * @author jason
+ * @date 2018-09-26
+ */
+public class ServiceException extends Exception {
+
+    private int code;
+    private String message;
+
+    private ServiceException(){}
+
+    public ServiceException(int code, String message){
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "ServiceException{" +
+                "code=" + code +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
