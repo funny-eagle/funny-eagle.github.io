18ec5888b831d12748ca77f64dca87928d11c3a1
yangjinlong <yangjinlong86@gmail.com>
2019-06-18 18:04:54 +0800

    add cnzz

--- src/components/layout.js
+++ src/components/layout.js
@@ -40,6 +40,7 @@ class Layout extends React.Component {
           <footer>
             <hr style={{marginBottom: 0}}/>
             <span style={{color: `grey`}}>nocoder.org © {new Date().getFullYear()}</span>
+            <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1257391581'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1257391581%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
           </footer>
         </div>
       </div>
