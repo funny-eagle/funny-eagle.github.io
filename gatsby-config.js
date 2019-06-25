a1a8a87e26c0e8cd358eb02772b05e3714b434a1
yangjinlong <yangjinlong86@gmail.com>
2019-06-25 17:05:34 +0800

    update blog title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "Jason Yang's Blog",
+    title: "Nocoder - Jason Yang's Blog",
     author: `Jason Yang`,
     description: `nocoder blog`,
     siteUrl: `http://nocoder.org`,
