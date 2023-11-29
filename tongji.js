876c74799aaf794aef016712713fc342b49fd2f2
yangjinlong <yangjl@travelsky.com.cn>
2023-11-29 16:59:11 +0800

    Updates

--- tongji.js
+++
@@ -1,7 +0,0 @@
-var _hmt = _hmt || [];
-          (function() {
-            var hm = document.createElement("script");
-            hm.src = "https://hm.baidu.com/hm.js?065ec4ac737fe35f7a9a97a6aabec697";
-            var s = document.getElementsByTagName("script")[0]; 
-            s.parentNode.insertBefore(hm, s);
-          })();
